d49318c999bd99348ddb3f87915b798e0e543917
Eden Mesfun <emesfun@gmail.com>
2018-03-23 01:08:06 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -85,7 +85,11 @@ class PigLatinizer
           split_word.shift
           new_word = split_word.push(word[0]).push("a", "y").join("")
           pig_latin << new_word
-        elsif 
+        elsif @consonants_caps.include?(word[0]) && word.length == 6 #TESTING, GULF
+          split_word = word.split("")
+          split_word.shift
+          new_word = split_word.push(word[0]).push("a", "y").join("")
+          pig_latin << new_word
         elsif @vowels.include?(word[0]) || @vowels_caps.include?(word[0]) && @consonants.include?(word[1])
           new_word = word + "way"
           pig_latin << new_word
