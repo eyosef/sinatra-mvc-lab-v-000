225c49928f2314aa840f0c622cb81696bf16850c
Eden Mesfun <emesfun@gmail.com>
2018-03-23 01:16:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -80,7 +80,7 @@ class PigLatinizer
         elsif @vowels.include?(word[0]) && word.length == 1
           new_word = word[0] + "way"
           pig_latin << new_word
-        elsif @consonants_caps.include?(word[0]) && word.length == 6 #TESTING, GULF
+        elsif @consonants_caps.include?(word[0]) && word.length == 6 
           split_word = word.split("")
           split_word.shift
           split_word.shift
