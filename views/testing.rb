52239ed3ec909b2d4c009f9572ef7ab3104ac791
Eden Mesfun <emesfun@gmail.com>
2018-03-23 01:05:55 +0000

    automatically backed up by learn

--- views/testing.rb
+++ views/testing.rb
@@ -3,8 +3,12 @@ hay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay
 ay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
    expected "eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay i
 nway ethay lfGuay reamStay andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway
-akingtay away ishfay" 
+akingtay away ishfay"
 
-to include "eHay asway anway oldway anmay owhay ishedfay aloneway in
-way away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysd
+to include "eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway 
+ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysd
 ay ownay ithoutway akingtay away ishfay"
+
+Output/Goal 
+lfGuay / ulfGay 
+reamStay / eamStray
