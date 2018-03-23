7f674250794671058a6019c16edd37896ad8842f
Eden Mesfun <emesfun@gmail.com>
2018-03-23 01:07:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -82,6 +82,8 @@ class PigLatinizer
           pig_latin << new_word
         elsif @consonants_caps.include?(word[0]) && word.length == 6
           split_word = word.split("")
+
+
         elsif @vowels.include?(word[0]) || @vowels_caps.include?(word[0]) && @consonants.include?(word[1])
           new_word = word + "way"
           pig_latin << new_word
